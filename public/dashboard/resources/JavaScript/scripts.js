4269fe5873c0bfd460802b8d4290ba1c4d99a09e
DarkModz-official <sebastian@schindlertai.de>
2022-05-16 18:55:30 +0200

    Passwort Check 🔏 Part A ClientSide hinzugefügt!

---
+++ public/dashboard/resources/JavaScript/scripts.js
@@ -0,0 +1,19 @@
+function pwdcheck() {
+    var pwd = document.getElementById("fpwd").value;
+    var pwd2 = document.getElementById("spwd").value;
+    
+    if (pwd != pwd2) {
+        document.getElementById("pwdcheck").innerHTML = "Die angegebenen Passwörter stimmen nicht überein!";
+        document.getElementById("pwdcheck").style.color = "red";
+        document.getElementById("btn-register").disabled = true;
+    } else {
+        if (pwd.length < 8) {
+            document.getElementById("pwdcheck").innerHTML = "Das Passwort muss mindestens 8 Zeichen lang sein!";
+            document.getElementById("pwdcheck").style.color = "red";
+            document.getElementById("btn-register").disabled = true;
+        } else {
+            document.getElementById("pwdcheck").innerHTML = " ";
+            document.getElementById("btn-register").disabled = false;
+        }
+    }
+}
