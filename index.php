3526957f70353185e98573983e32a316135b3b6b
KeksGauner <darklupo5.2003@gmail.com>
2022-05-17 00:00:42 +0200

    Added: Error 404 Seite

--- index.php
+++ index.php
@@ -71,18 +71,6 @@ else
   //Keine Route gefunden darum 404 Seite senden
   http_response_code(404);
   header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
-  echo '
-  <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-  <html>
-    <head>
-        <title>404 Not Found</title>
-    </head>
-    <body>
-        <h1>Not Found</h1>
-        <p>The requested URL '. htmlspecialchars($_SERVER['REQUEST_URI']) .' was not found on this server.</p>
-    </body>
-  </html>
-  ';
-  exit;
+  die(require ROOTPATH . '/public/error/404.php');
 }
 ?>
